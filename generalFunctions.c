6f848f79f39a3d9e9972daabc38a75333eca6335
Eduardo Pérez <69828130+tiero-x@users.noreply.github.com>
2020-12-02 14:06:48 -0600

    Update generalFunctions.c

--- generalFunctions.c
+++ generalFunctions.c
@@ -15,8 +15,7 @@
 #include "generalFunctions.h"
 
 //Estructura de la tabla de información
-typedef struct imageInformation
-{
+typedef struct imageInformation {
   short int *sectorSize;
   int numberOfSectorsPerCluster;
   short int *reservedSectors;
@@ -28,7 +27,6 @@ typedef struct imageInformation
   char idSistema[11];
   int rootDirectoryAddress;
   int addressImageInfoBegins;
-
 } imageInformation;
 
 int fd; // Archivo a leer
@@ -38,12 +36,10 @@ char *map;
 imageInformation imageInfo;
 
 //Función que mapea el archivo
-char *mapFile(char *filePath)
-{
+char *mapFile(char *filePath, long *fs) {
   /* Abre archivo */
   fd = open(filePath, O_RDWR);
-  if (fd == -1)
-  {
+  if (fd == -1) {
     system("clear");
     perror("\nError abriendo el archivo\n");
     getchar();
@@ -53,11 +49,10 @@ char *mapFile(char *filePath)
   /* Mapea archivo */
   struct stat st;
   fstat(fd, &st);
-  fs = st.st_size;
+  *fs = st.st_size;
 
-  char *map = mmap(0, fs + 200, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-  if (map == MAP_FAILED)
-  {
+  char *map = mmap(0, *fs, PROT_READ, MAP_SHARED, fd, 0);
+  if (map == MAP_FAILED) {
     close(fd);
     system("clear");
     perror("\nError mapeando el archivo\n");
@@ -67,208 +62,265 @@ char *mapFile(char *filePath)
   return map;
 }
 
-int leeChar()
-{
+int leeChar(){
   int chars[5];
-  int ch, i = 0;
-  nodelay(stdscr, TRUE);
-  while ((ch = getch()) == ERR)
-    ; //Espera activa
+  int ch,i=0;
+  nodelay(stdscr,TRUE);
+  while((ch=getch()) == ERR); //Espera activa
   ungetch(ch);
-  while ((ch = getch()) != ERR)
-  {
-    chars[i++] = ch;
+  while((ch=getch()) != ERR){
+    chars[i++]=ch;
   }
   //Convierte a número con todo lo leído
-  int res = 0;
-  for (int j = 0; j < i; j++)
-  {
-    res <<= 8;
+  int res=0;
+  for(int j=0;j<i;j++){
+    res<<=8;
     res |= chars[j];
   }
 }
 
-  //Función que
-  int getNext(int cluster, int base)
-  {
-    // Para FAT12
-    int offset = cluster + cluster / 2;
-    int flag = cluster % 2; // Nos dice si en la parte baja o alta
+//Función que 
+int getNext(int cluster, int base) {
+  // Para FAT12
+  int offset = cluster + cluster / 2;
+  int flag = cluster % 2; // Nos dice si en la parte baja o alta
 
-    unsigned char b1, b2;
-    printf("%x", map);
-    b1 = map[base + offset];
-    printf("%02x:", b1);
-    b2 = map[base + offset + 1];
-    printf("%02x\n", b2);
-    int result = b1 | b2 << 8; // Los bits mas significativos van al final
+  unsigned char b1, b2;
+  b1 = map[base + offset];
+  printf("%02x:", b1);
+  b2 = map[base + offset + 1];
+  printf("%02x\n", b2);
+  int result = b1 | b2 << 8; // Los bits mas significativos van al final
 
-    if (flag)
-    {
-      result >>= 4;
-    }
-    else
-    {
-      result &= 0xfff;
-    }
-
-    printf("%04x\n", result);
-
-    return result;
+  if (flag) {
+    result >>= 4;
+  } else {
+    result &= 0xfff;
   }
 
-  void readCluster(int cluster, char *buffer)
-  {
-    int offset = cluster * 512;
-    memcpy(buffer, &map[0x3E00 + offset], 512);
-  }
+  printf("%04x\n", result);
 
-  void leeArchivo(char *nombre, long tam, int cluster)
-  {
+  return result;
+}
+
+int getNext16(int cluster, int base){
+  //Para FAT 16
+  int offset=cluster*2;
+  
+  unsigned short int b;
+  b=*(short int *)&map[base + offset];
+  int result = b;
+  
+  return result;
+}
+
+int getNext32(int cluster, int base){
+  //Para FAT 32
+  int offset=cluster*4;
+  
+  unsigned int b;
+  b=*(int *)&map[base + offset];
+  int result = b;
+  
+  return result;
+}
+
+void readCluster(int cluster, char *buffer, int inicio, int size){
+    int offset = cluster*size*512;
+    memcpy(buffer, &map[inicio+offset],size*512);
+}
+
+void leeArchivo(char *nombre, long tam, int cluster){
     int fo = open(nombre, O_WRONLY | O_CREAT, 0x755);
     char buffer[512];
     int te;
-    do
-    {
-      readCluster(cluster, buffer);
-      cluster = getNext(cluster, 0x200);
-      te = (tam > 512) ? 512 : tam;
-      write(fo, buffer, te);
-      tam -= 512;
-    } while (tam > 0);
+    do{
+        readCluster(cluster,buffer,0x3E00);
+        cluster = getNext(cluster,0x200);
+        te=(tam>512) ? 512 : tam;
+        write(fo,buffer,te);
+        tam -= 512;
+    } while(tam>0);
     close(fo);
-  }
+}
 
-  int MBR(char *base)
-  {
-    int res = 1;
-    int i = 0;
+void copiaMem(char *map, int cluster, long tam, int inicio, int size){
+  do{
+    readCluster(cluster, map, inicio, size);
+    cluster=getNext(cluster, 0x200);
+    tam-=512;
+    map+=512;
+  } while (tam>0);
+}
+ 
+void copiaMem16(char *map, int cluster, long tam, int inicio, int size){
+  do{
+    readCluster(cluster, map, inicio, size);
+    cluster=getNext16(cluster,0x1000);
+    tam-=512;
+    map+=512;
+  } while (tam>0);
+}
 
-    if (base[510] != 0x55 && base[511] != 0xAA)
-      res = 0;
-
-    while (res && i < 4)
-    {
-      int p = 0x1BE + i * 16;
-      if (!(base[p] == 0 || base[p] == 0x80))
-        res = 0;
-      i++;
+int MBR(char *base){
+    int res=1;
+    int i=0;
+    
+    //Checa firma
+    if(base[510] != 0x55 && base[511] != 0xAA){
+        res=0;
+    }
+    
+    //Checa que haya al menos una partición
+    if(res && base[0x1BF]==0 && base[0x1C0]==0 && base[0x1C1]==0){
+        res=0;
+    }
+    
+    while(res && i<4){
+        int p = 0x1BE + i*16;
+        if(!(base[p] == 0 || base[p] == 0x80)) res=0;
+        i++;
     }
     return res;
-  }
+}
 
-  // Función que
-  void pruebas()
-  {
+// Función que Abre el archivo
+void leeInfo(char *map){
+  system("clear");
+  printf("\n    Tabla de información de la imagen\n");
+  printf("--------------------------------------------\n\n");
+  imageInfo.sectorSize = (short int *)&map[11];
+  printf("Tamaño del  sector:              %d \n", *imageInfo.sectorSize);
+
+  imageInfo.numberOfSectorsPerCluster = map[13];
+  printf("Numero de sectores por cluster:  %d \n", imageInfo.numberOfSectorsPerCluster);
+
+  imageInfo.reservedSectors = (short int *)&map[14];
+  printf("Sectores reservados:             %d \n", *imageInfo.reservedSectors);
+
+  imageInfo.numberOfCopiesofFat = map[16];
+  printf("Número de copias del FAT:        %d \n", imageInfo.numberOfCopiesofFat);
+
+  imageInfo.numberOfEntriesRootDirectory = (short int *)&map[17];
+  printf("Entradas directorio Raíz:        %d \n", *imageInfo.numberOfEntriesRootDirectory);
+
+  imageInfo.numberOfDiskSectors = (int *)&map[32];
+  printf("Número de sectores del disco:    %d \n", *imageInfo.numberOfDiskSectors);
+
+  imageInfo.fatSize = (short int *)&map[22];
+  printf("Tamaño del FAT:                  %d \n", *imageInfo.fatSize);
+
+  strcpy(imageInfo.volumenLabel, &map[43]);
+  printf("Etiqueta del Volumen:            %s \n ", imageInfo.volumenLabel);
+
+  strcpy(imageInfo.idSistema, &map[0x36]);
+  printf("ID Sistema:                      %s \n ", imageInfo.idSistema);
+
+  imageInfo.rootDirectoryAddress = (*imageInfo.reservedSectors + (imageInfo.numberOfCopiesofFat * *imageInfo.fatSize)) * *imageInfo.sectorSize;
+  imageInfo.addressImageInfoBegins = imageInfo.rootDirectoryAddress + (*imageInfo.numberOfEntriesRootDirectory * 32);
+
+  printf("\nDireccion del directorio raiz: 0x%04x\n", imageInfo.rootDirectoryAddress);
+  printf("\nDireccion donde comienza la información de archivos en la imagen: 0x%04x\n", imageInfo.addressImageInfoBegins);
+
+  printf("\n\t\t[Press enter to continue]\n\n");
+  getchar();
+  
+    //Es MBR
+       if(MBR(map)){
+         attron(A_REVERSE);
+         mvprintw(4,40,"Es MBR");
+         attroff(A_REVERSE);
+         
+         leeChar();
+         clear();
+         
+         move(4,5);
+         addstr("Partición C  H  S Sector");
+         for(int i=0;i<4;i++){
+           int h=(byte)map[0x1BE + i*16];
+           mvprintw(5+i,20,"%d",h);
+           int s=map[0x1BE + i*16 + 2] & 0x3F;
+           int c=map[0x1BE + i*16 + 2] & 0xC0;
+           c<<=2;
+           mvprintw(5+i,25,"%d",s);
+           c |= map[0x1BE + i*16 + 3];
+           mvprintw(5+i,15,"%d",c);
+           int sec=(c*255 + h)*63 +(s-1);
+           mvprintw(5+i,30,"%d",sec);
+         }
+       }
+}
+
+void leeDirectorio(int d){
+    clear();
+    char *dir = &map[d];
+    char nombre[14];
+    int tipo;
+    int cluster;
+    unsigned int tam;
+    mvprintw(4,5,"Nombre");
+    mvprintw(4,20,"Tipo");
+    mvprintw(4,5,"Cluster");
+    mvprintw(4,5,"Tamaño");
+    int i=0, j;
+    while(i<25 && dir[i*32] != 0) j=0;
+    strncpy(nombre, &dir[i*32],13);
+    while(nombre[j] != ' ' && j<8) j++;
+    strncpy (&nombre[j+1],&dir[8 + i*32],3);
+    nombre[j+4]=0;
+    tipo=dir[0xb + i*32];
+    cluster=*((short int*) &dir[0x1a + i*32]);
+    int o = 0x1c + i*32;
+    tam=*(unsigned int*) &dir[0x1c + 32];
+    mvprintw(5+i,5,"%s",nombre);
+    mvprintw(5+i,20,"%d",tipo);
+    mvprintw(5+i,25,"%d",cluster);
+    mvprintw(5+i,35,"%ld",tam);
+    i++;
+}
+
+// Función que
+void pruebas(){
     int d;
-    for (int i = 0; i < 10; i++)
-    {
-      d = getNext(i, 0x200);
-      printf("%03x  %d\n", d, d);
-    }
-  }
-
-  // Función que Abre el archivo e imprime su información
-  void abre(char *filename)
-  {
-    map = mapFile(filename);
-    if (map == NULL)
-    {
-      exit(EXIT_FAILURE);
-    }
-
-    //pruebas();
-    system("clear");
-    printf("\n    Tabla de información de la imagen\n");
-    printf("--------------------------------------------\n\n");
-    imageInfo.sectorSize = (short int *)&map[11];
-    printf("Tamaño del  sector:              %d \n", *imageInfo.sectorSize);
-
-    imageInfo.numberOfSectorsPerCluster = map[13];
-    printf("Numero de sectores por cluster:  %d \n", imageInfo.numberOfSectorsPerCluster);
-
-    imageInfo.reservedSectors = (short int *)&map[14];
-    printf("Sectores reservados:             %d \n", *imageInfo.reservedSectors);
-
-    imageInfo.numberOfCopiesofFat = map[16];
-    printf("Número de copias del FAT:        %d \n", imageInfo.numberOfCopiesofFat);
-
-    imageInfo.numberOfEntriesRootDirectory = (short int *)&map[17];
-    printf("Entradas directorio Raíz:        %d \n", *imageInfo.numberOfEntriesRootDirectory);
-
-    imageInfo.numberOfDiskSectors = (int *)&map[32];
-    printf("Número de sectores del disco:    %d \n", *imageInfo.numberOfDiskSectors);
-
-    imageInfo.fatSize = (short int *)&map[22];
-    printf("Tamaño del FAT:                  %d \n", *imageInfo.fatSize);
-
-    strcpy(imageInfo.volumenLabel, &map[43]);
-    printf("Etiqueta del Volumen:            %s \n ", imageInfo.volumenLabel);
-
-    strcpy(imageInfo.idSistema, &map[0x36]);
-    printf("Id Systema:                      %s \n ", imageInfo.idSistema);
-
-    imageInfo.rootDirectoryAddress = (*imageInfo.reservedSectors + (imageInfo.numberOfCopiesofFat * *imageInfo.fatSize)) * *imageInfo.sectorSize;
-    imageInfo.addressImageInfoBegins = imageInfo.rootDirectoryAddress + (*imageInfo.numberOfEntriesRootDirectory * 32);
-
-    printf("\nDireccion del directorio raiz: 0x%04x\n", imageInfo.rootDirectoryAddress);
-    printf("\nDireccion donde comienza la información de archivos en la imagen: 0x%04x\n", imageInfo.addressImageInfoBegins);
-
-    printf("\n\t\t[Press enter to continue]\n\n");
-    getchar();
-
-    if (munmap(map, fs) == -1)
-    {
-
-      system("clear");
-      perror("\nError al desmapear\n");
-      getchar();
-    }
-    close(fs);
-  }
-  //Funciono
-  /*
-  int partitions(){
-   int a=0, a2=0;
-  if(MBR(map)){
-   printf("\nEs MBS\n\n");
-
-   for(int i=0; i<=3; i++)
-   {
-    int h =(byte)map[0x1BE + 1 + (i*16)];
-    if(h!=0){
-    printf("CHS Paricion %d\n", i+1);
-    printf("Head: %02x\n", h);
-    int s = map[0x1BE +  2 + (i*16)] & 0x3F;
-    int c = map[0x1BE + 2 + (i*16)] & 0xC0;
-     c<<=2;
-    printf("Sector: %02x\n", s);
-    c |= map[0x1BE + 3 + (i*16)];
-    printf("Cylinder: %02x\n",c);
-    a= ((c*255 + h)*63 +(s-1))*512;
-    printf("Partition Start: %02x\n\n", a);
+    d=leeChar();
+        
+    if(d=='1'){
+        leeInfo(map);
+        leeChar();
+        leeDirectorio(0x2600);
+        leeChar();
+        char *archmem=malloc(1024);
+        copiaMem(archmem,3,830,0x3E00,1);
+        edita(archmem,830);
     
-    int h2 =(byte)map[a+0x1BE + 1];
-    if(h2!=0)
-    {
-    printf("Partition Extended\n");
-    printf("Head: %02x\n", h2);
-    int s2 = map[a+0x1BE + 2] & 0x3F;
-    int c2 = map[a+ 0x1BE + 2] & 0xC0;
-     c2<<=2;
-    printf("Sector: %02x\n", s2);
-    c2 |= map[a+ 0x1BE + 3];
-    printf("Cylinder: %02x\n",c2);
-    a2= ((c2*255 + h2)*63 +(s2-1))*512;
-    printf("Partition Extended Start: %02x\n\n", a2);
-    leeChar(a2);
     }else{
-      leeChar(a);
+        leeInfo(map);
+        leeChar();
+        leeInfo(&map[0x100000]);
+        leeChar();
+        leeDirectorio(0x141000);
+        leeChar();
+        map=&map[0x100000];
+        char *archmem=malloc(4096);
+        copiaMem16(archmem,2,302,0x44000,8);
+        edita(archmem,302);
     }
+}
+
+// Función que Abre el archivo
+int abre(char *filename) {
+    long fs;
+    char *map = mapFile(filename,&fs);
+    if (map == NULL) {
+        exit(EXIT_FAILURE);
     }
-   }
-  }else{
-    printf("NO ES MBS\n");
-  }
-  */
+    
+    pruebas();
+    
+    if(munmap(map,fd)== -1){
+        system("clear");
+        perror("Error al desmapear");
+    }
+    close(fd);
+    return 0;
 }
